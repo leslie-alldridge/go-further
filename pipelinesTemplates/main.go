7207293f5b86994863c7d239402d11090753f228
Leslie Alldridge <leslie.alldridge@gmail.com>
2019-01-28 05:39:32 +1300

    pipelines and functions for templates
    pipelines and functions for templates

---
+++ pipelinesTemplates/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"log"
+	"os"
+	"time"
+	"text/template"
+)
+
+var tpl *template.Template
+
+func init() {
+	tpl = template.Must(template.New("").Funcs(fm).ParseFiles("index.gohtml"))
+}
+
+func monthDayYear(t time.Time) string {
+	return t.Format("01-02-2006")
+}
+
+var fm = template.FuncMap{
+	"fdateMDY": monthDayYear,
+}
+
+func main(){
+	err := tpl.ExecuteTemplate(os.Stdout, "index.gohtml", time.Now())
+	if err != nil {
+		log.Fatal(err)
+	}
+}
