787f69b577e14d706d3a281d5d674ca8fb77b581
Leslie Alldridge <leslie.alldridge@gmail.com>
2019-01-27 18:26:45 +1300

    parsing glob and files with data/variables
    parsing glob and files with data/variables

--- templates/main.go
+++ templates/main.go
@@ -10,16 +10,20 @@ var tpl *template.Template
 
 func init() {
 	// adds entire templates folder to be parsed 
-	tpl = template.Must(template.ParseGlob("templates/*")) 
+	tpl = template.Must(template.ParseGlob("template/*")) 
+
+	//Singular file
+	// tpl = template.Must(template.ParseFiles("tpl.gohtml"))
 }
 
 func main()  {
-	err := tpl.Execute(os.Stdout, nil)
+	//data
+	err := tpl.ExecuteTemplate(os.Stdout, "data.gohtml", 42)
 	if err != nil {
 		log.Fatalln(err)
 	}
-
-	err = tpl.ExecuteTemplate(os.Stdout, "index.gohtml", nil)
+	//variable
+	err = tpl.ExecuteTemplate(os.Stdout, "variable.gohtml", "Focus on being your best")
 	if err != nil {
 		log.Fatalln(err)
 	}
