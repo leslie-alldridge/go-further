572686258667d2d2fb02db0c9ecaf2d2100703bc
Leslie Alldridge <leslie.alldridge@gmail.com>
2019-01-28 20:03:04 +1300

    starting servers
    starting servers

---
+++ servers/tcpServer/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+	"bufio"
+	"log"
+	"net"
+)
+
+func main()  {
+	li, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		log.Panic(err)
+	}
+	defer li.Close()
+
+	for {
+		conn, err := li.Accept()
+		if err != nil {
+			log.Println(err)
+		}
+		go handle(conn)
+	}
+}
+
+func handle(conn net.Conn) {
+	scanner := bufio.NewScanner(conn)
+	for scanner.Scan() {
+		ln := scanner.Text()
+		fmt.Println(ln)
+	}
+	defer conn.Close()
+}
+
+//Printed out the following
+
+// GET / HTTP/1.1
+// Host: localhost:8080
+// User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:64.0) Gecko/20100101 Firefox/64.0
+// Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8
+// Accept-Language: en-US,en;q=0.5
+// Accept-Encoding: gzip, deflate
+// Connection: keep-alive
+// Upgrade-Insecure-Requests: 1
